cf55e1f26b133fa524b4aafbd82298408f15b8c1
Tom Serenander <tom@serenander.se>
2023-11-29 17:20:23 +0100

    First draft of start page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Menu from './components/Menu/index.js'
 
 function App() {
 	return (
-		<>
+		<main>
 			<Menu onOpen={() => console.log('Opened/closed')}>
 				<Menu.Button>Click here</Menu.Button>
 				<Menu.Dropdown>
@@ -12,7 +12,10 @@ function App() {
 					<Menu.Item>Testimonials</Menu.Item>
 				</Menu.Dropdown>
 			</Menu>
-		</>
+
+			<h1>React Component Library</h1>
+			<p>Select component from the menu at the left</p>
+		</main>
 	)
 }
 
