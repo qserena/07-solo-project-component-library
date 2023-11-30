e06c1a0f3ee2500ce994a752b1276106f7a3ba8f
Tom Serenander <tom@serenander.se>
2023-11-30 21:38:26 +0100

    More Banners.

--- src/pages/BannersPage.jsx
+++ src/pages/BannersPage.jsx
@@ -18,114 +18,48 @@ export default function BannersPage() {
 					<h2>Success</h2>
 				</span>
 
-				<div className="badge--container">
-					<Banner title="Congratulations!" variant="success">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Aliquid pariatur, ipsum similique veniam.
-					</Banner>
-				</div>
-				<div className="badge--container">
-					<Banner title="Congratulations!" variant="success" />
-				</div>
+				<Banner title="Congratulations!" variant="success">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Aliquid pariatur, ipsum similique veniam.
+				</Banner>
+
+				<Banner title="Congratulations!" variant="success" />
 
 				<h2>Warning</h2>
 
-				<div className="badge--container">
-					<Banner title="Attention" variant="warning">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Aliquid pariatur, ipsum similique veniam quo totam eius
-						aperiam dolorum.
-					</Banner>
-				</div>
-				<div className="badge--container">
-					<Banner title="Attention" variant="warning" />
-				</div>
+				<Banner title="Attention" variant="warning">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Aliquid pariatur, ipsum similique veniam quo totam eius
+					aperiam dolorum.
+				</Banner>
+
+				<Banner title="Attention" variant="warning" />
 
 				<h2>Error</h2>
 
-				<div className="badge--container">
-					<Banner
-						title="There is a problem with your application"
-						variant="error"
-					>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Aliquid pariatur, ipsum similique veniam quo totam eius
-						aperiam dolorum.
-					</Banner>
-				</div>
-				<div className="badge--container">
-					<Banner
-						title="There is a problem with your application"
-						variant="error"
-					/>
-				</div>
+				<Banner
+					title="There is a problem with your application"
+					variant="error"
+				>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Aliquid pariatur, ipsum similique veniam quo totam eius
+					aperiam dolorum.
+				</Banner>
+
+				<Banner
+					title="There is a problem with your application"
+					variant="error"
+				/>
 
 				<h2>Neutral</h2>
 
-				<div className="badge--container">
-					<Banner title="Update available" variant="neutral">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Aliquid pariatur, ipsum similique veniam.
-					</Banner>
-				</div>
-				<div className="badge--container">
-					<Banner title="Update available" variant="neutral" />
-				</div>
-				{/* <div className="badge--container">
-					<p className="badge--color-text">Yellow</p>
-					<Badge text="Badge" color="yellow" shape="square" />
-				</div>
-				<div className="badge--container">
-					<p className="badge--color-text">Green</p>
-					<Badge text="Badge" color="green" shape="square" />
-				</div>
-				<div className="badge--container">
-					<p className="badge--color-text">Blue</p>
-					<Badge text="Badge" color="blue" shape="square" />
-				</div>
-				<div className="badge--container">
-					<p className="badge--color-text">Indigo</p>
-					<Badge text="Badge" color="indigo" shape="square" />
-				</div>
-				<div className="badge--container">
-					<p className="badge--color-text">Purple</p>
-					<Badge text="Badge" color="purple" shape="square" />
-				</div>
-				<div className="badge--container">
-					<p className="badge--color-text">Pink</p>
-					<Badge text="Badge" color="pink" shape="square" />
-				</div> */}
-			</div>
+				<Banner title="Update available" variant="neutral">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Aliquid pariatur, ipsum similique veniam.
+				</Banner>
 
-			{/* <div className="badge--shape-container">
-				<span className="badge--shape-text">
-					<h2>Pill</h2>
-				</span>
-				<div className="badge--container">
-					<Badge text="Badge" color="gray" shape="pill" />
-				</div>
-				<div className="badge--container">
-					<Badge text="Badge" color="red" shape="pill" />
-				</div>
-				<div className="badge--container">
-					<Badge text="Badge" color="yellow" shape="pill" />
-				</div>
-				<div className="badge--container">
-					<Badge text="Badge" color="green" shape="pill" />
-				</div>
-				<div className="badge--container">
-					<Badge text="Badge" color="blue" shape="pill" />
-				</div>
-				<div className="badge--container">
-					<Badge text="Badge" color="indigo" shape="pill" />
-				</div>
-				<div className="badge--container">
-					<Badge text="Badge" color="purple" shape="pill" />
-				</div>
-				<div className="badge--container">
-					<Badge text="Badge" color="pink" shape="pill" />
-				</div>
-			</div> */}
+				<Banner title="Update available" variant="neutral" />
+			</div>
 		</div>
 	)
 }
