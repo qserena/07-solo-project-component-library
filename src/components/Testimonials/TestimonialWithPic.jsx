05a45adfc5e796f9450991941149c56b9d5838f6
Tom Serenander <tom@serenander.se>
2023-12-02 10:10:22 +0100

    Testimonial with picture looks good.

--- src/components/Testimonials/TestimonialWithPic.jsx
+++ src/components/Testimonials/TestimonialWithPic.jsx
@@ -1,13 +1,17 @@
 import classnames from 'classnames'
+import quoteIcon from '../../assets/quote-icon.png'
 
 export default function TestimonialWithPic({ quote, name, role, className }) {
 	const allClasses = classnames('testimonial--with-pic', className)
 	return (
 		<div className={allClasses}>
 			<div className="testimonial--picture"></div>
-			<p>{quote}</p>
-			<p>{name}</p>
-			<p>{role}</p>
+			<div className="testimonial--text-container">
+				<img src={quoteIcon} alt="quote icon" />
+				<p className="testimonial--quote">{quote}</p>
+				<p className="testimonial--name">{name}</p>
+				<p className="testimonial--role">{role}</p>
+			</div>
 		</div>
 	)
 }
